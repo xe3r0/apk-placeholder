c24c36b51e4a3733535c6dabc16b1f84416cee71
xe3r0 <82065375+xe3r0@users.noreply.github.com>
2026-01-30 07:56:25 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.google.android.safetycore"
+    namespace = "com.google.android.contactkeys"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.google.android.safetycore"
+        applicationId = "com.google.android.contactkeys"
         minSdk = 24
         targetSdk = 34
         versionCode = 2000000000
